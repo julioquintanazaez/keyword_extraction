b49ee67e1b676092188da1deb070f342ad8c0029
julioquintanazaez <jupyterdevs2022@gmail.com>
2024-11-21 22:01:49 -0500

    First commit of the app

---
+++ main.py
@@ -0,0 +1,87 @@
+from fastapi import FastAPI, File, UploadFile  
+from keybert import KeyBERT
+import networkx as nx
+
+import sys
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+# Create an instance for the app
+app = FastAPI()
+
+# Initializate KeyBERT instance
+kw_model = None
+graph = None
+
+def initialize_bert_model():
+  global kw_model 
+  # Cargamos una instancia del modelo BERT  
+  kw_model = KeyBERT()
+  print("Modelo inicializado")
+
+def initialize_graph():
+  global graph
+  # Cargamos una instancia del Graph 
+  graph = nx.Graph()
+  print("Grafo inicializado")
+
+def copy_lines(contents):
+  count = -1
+  data_lines = []
+  lines = contents.decode('utf-8').splitlines()
+  for line in lines:
+    if line.startswith(">"):                
+      data_lines.append(line)
+      count = count + 1
+    else:
+      data_lines[count] = data_lines[count] + line 
+  return data_lines
+
+@app.on_event("startup")
+async def startup_event():
+  initialize_bert_model()
+  initialize_graph()
+
+@app.get('/')
+async def index():
+  return { "message": "This is a keyword extraction app with linked words from multiple documents!" }
+
+@app.post("/upload_documents_txt/")
+async def upload_documents_txt(file: UploadFile = File(...)):
+    # Verificamos que el archivo sea un CSV  
+    if file.content_type != 'text/plain':
+        return {"error": "El archivo debe ser un CSV."}
+    # Leemos el archivo CSV en un DataFrame de pandas  
+    try:
+      contents = await file.read()
+      data_lines = copy_lines(contents)
+
+      # procesamos los datos para extraer las keywords
+      if kw_model is None:
+        return {"error": "El modelo no está inicializado."} 
+
+      key_dict = {}
+      for n, line in enumerate(data_lines):
+        keywords = kw_model.extract_keywords(line, keyphrase_ngram_range=(1, 3), stop_words=None) #, keyphrase_ngram_range=(1, 2), stop_words=None   
+        key_dict[f"document_{n}"] = keywords    
+        # Enlazar keyword que pertenecen a un mismo documento
+        for i, a_word in enumerate(keywords):
+          for j, b_word in enumerate(keywords):
+            if j > i:
+              if graph.has_edge(a_word[0], b_word[0]) != True:
+                graph.add_edge(a_word[0], b_word[0], document_count=1)
+              else:
+                try:
+                  document_att = nx.get_edge_attributes(graph, "document_count")                  
+                  att_count_value = document_att[(a_word[0], b_word[0])]
+                  print(f"{a_word[0]} + {b_word[0]}: {att_count_value}")
+                  graph.add_edge(a_word[0], b_word[0], document_count = (att_count_value + 1))                
+                except Exception as e:
+                  return {"error": f"Procesando atributos de la conexión: {str(e)}"}
+
+      list_edges = list(graph.edges(data=True))
+      return {"Keywords": key_dict, "Graph": list_edges}
+    except Exception as e:
+        return {"error": f"No se pudo procesar el archivo: {str(e)}"}
